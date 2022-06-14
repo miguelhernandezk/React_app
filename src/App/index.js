b311b1912d07554c164b4b2c43fd0b2fde0657ef
Miguel Hernández <miguelhernandezk@gmail.com>
2022-06-14 13:12:44 -0500

    Lesson 7: Component composition without React Context

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,73 @@
 import React from "react";
-import { AppUI } from "./AppUI";
-import { ToDoProvider } from "../ToDoContext"
+import { useToDos } from "./useToDos"
+import { ToDoCounter } from "../ToDoCounter";
+import { ToDoSearch } from "../ToDoSearch";
+import { ToDoList } from "../ToDoList";
+import { ToDoItem } from "../ToDoItem";
+import { CreateToDoButton } from "../CreateToDoButton";
+import { Modal } from "../Modal"
+import { ToDoForm } from "../ToDoForm"
+import { ToDosError } from "../ToDosError";
+import { ToDosLoading } from "../ToDosLoading";
+import { EmptyToDos } from "../EmptyToDos";
+import { ToDoHeader } from "../ToDoHeader";
 
 // import './App.css';
 
 function App() {
-  return (
-    <ToDoProvider>
-      <AppUI/>
-    </ToDoProvider>
+  const { error,
+    loading,
+    searchedToDos, 
+    completeToDo, 
+    deleteToDo,
+    openModal,
+    setOpenModal,
+    totalToDos, 
+    completedToDos,
+    searchValue, 
+    setSearchValue,
+    addToDo, 
+} = useToDos();
+return(
+  <React.Fragment>
+    <ToDoHeader>
+      <ToDoCounter
+        totalToDos = { totalToDos }
+        completedToDos = { completedToDos }
+      />
+      <ToDoSearch
+        searchValue = { searchValue }
+        setSearchValue = { setSearchValue }
+      />
+    </ToDoHeader>
+  
+      <ToDoList>
+          {error && <ToDosError error={error}/>}
+          {loading && <ToDosLoading/>}
+          {(!loading && !searchedToDos.length) && <EmptyToDos/>}
+
+          {searchedToDos.map(toDo => (
+          <ToDoItem 
+              key={toDo.text} 
+              text={toDo.text} 
+              completed={toDo.completed} 
+              onComplete={() => completeToDo(toDo.text)}
+              onDelete={() => deleteToDo(toDo.text)}
+              />
+          ))} 
+      </ToDoList>
+      {!!openModal && (
+        <Modal>
+          <ToDoForm 
+            addToDo = { addToDo }
+            setOpenModal = { setOpenModal }
+          />
+        </Modal>
+      )}
+    <CreateToDoButton
+      setOpenModal={setOpenModal}
+    />
+  </React.Fragment>
   );
 }
 
