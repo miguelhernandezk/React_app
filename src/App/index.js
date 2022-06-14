4a61d2c76be2f775d57233c30026ea9287e34da4
Miguel Hernández <miguelhernandezk@gmail.com>
2022-06-14 00:48:58 -0500

    Files restored

--- src/App/index.js
+++ src/App/index.js
@@ -6,49 +6,12 @@ import { ToDoCounter } from "../ToDoCounter";
 // import './App.css';
 
 
-function App(){
-  return(
-    <React.Fragment>
-      <ToDoHeader/>
-      <TodoList/>
-    </React.Fragment>
+function App() {
+  return (
+    <ToDoProvider>
+      <AppUI/>
+    </ToDoProvider>
   );
-}
-
-function ToDoHeader(){
-  return(
-    <React.Fragment>
-      <ToDoCounter/>
-      <ToDoSearch/>
-    </React.Fragment>
-  );
-}
-
-function ToDoList(){
-  return(
-    <React.Fragment>
-      <ToDoItems/>
-    </React.Fragment>
-  );
-}
-
-function ToDoCounter(){
-  return <p>ToDoCounter</p>;
-}
-
-function ToDoSearch(){
-  return <p>ToDoSearch</p>;
-}
-
-function ToDoItem(){
-  return ;
-}
-// function App() {
-//   return (
-//     <ToDoProvider>
-//       <AppUI/>
-//     </ToDoProvider>
-//   );
 }
 
 export default App;
